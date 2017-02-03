debde109dbf19c7bc89701e3b060e1cd4c5a37d0
Ted Weber <webert3@students.wwu.edu>
2017-02-03 08:55:01 -0800

    Working on building AST inside of the parse function.

--- parser.pl
+++ parser.pl
@@ -3,54 +3,58 @@
 
 % Parser
 %	Input: List of tokenized values returned from scanner.
-%	Output: True if valid syntax, false otherwise.
+%	Returns: AST of the form [Num, [Commands]].
+%%
+parse(Tokens, AST) :- program(Tokens, [[punctuation,')'],[punctuation,eof]], AST).
 
-parse(Tokens) :- program(Tokens, []).
-
-program(Tokens, Rest) :- open_brack(Tokens, T1), 
+program(Tokens, Rest, [NumArgs|AST]) :- open_brack(Tokens, T1), 
 						postfix(T1, T2), 
 						num_args(T2, NumArgs, T3),
-						command_sequence(T3, Rest).
+						command_sequence(T3, AST, Rest).
 
 
-% check_args(NumArgs, Args, Rest).
+% Handles command sequences
+%%
+command_sequence(List, [AST_Head|AST_Tail], Rest) :- command(List, AST_Head, List1),write(AST_Head),nl,
+													command_sequence(List1,AST_Tail,Rest).
+command_sequence(List, AST, List). 
+
+% Handles all valid command tokens.
+%%
+command([H|T],AST_Head, T) :- member(number, H),get_tail(H,AST_Head);
+					member(add, H),get_tail(H,AST_Head);
+					member(sub, H),get_tail(H,AST_Head);
+					member(mul, H),get_tail(H,AST_Head);
+					member(div, H),get_tail(H,AST_Head);
+					member(rem, H),get_tail(H,AST_Head);
+					member(lt, H),get_tail(H,AST_Head);
+					member(eq, H),get_tail(H,AST_Head);
+					member(gt, H),get_tail(H,AST_Head);
+					member(pop, H),get_tail(H,AST_Head);
+					member(swap, H),get_tail(H,AST_Head);
+					member(sel, H),get_tail(H,AST_Head);
+					member(nget, H),get_tail(H,AST_Head);
+					member(exec, H),get_tail(H,AST_Head).
+command([[punctuation,'(']|T], Rest) :- command_sequence(T, [[punctuation,')']|Rest]).
 
 % Checks for opening bracket
+%%
 open_brack([H|T], T) :- member('(',H).
 
 % Checks for 'postfix' 
+%%
 postfix([H|T], T) :- member(postfix, H).
 
-% Checks for a number of arguments specified.
-	% Probably do not need NumArgs anymore
+% Checks that an argument number is specified.
+%	Input: Token list
+% 	Output: Rest of tokens
+%			Number of arguments.
+%%
 num_args([H|T], NumArgs, T) :- member(number,H),
 								get_tail(H, NumArgs).
 
-% Handles command sequences
-command_sequence(List, Rest) :- command(List, List1),
-								command_sequence(List1,Rest). 
-command_sequence(List, List).
-
-
-% Handles all valid command tokens.
-command([H|T], T) :- member(number, H);
-					member(add, H);
-					member(sub, H);
-					member(mul, H);
-					member(div, H);
-					member(rem, H);
-					member(lt, H);
-					member(eq, H);
-					member(gt, H);
-					member(pop, H);
-					member(swap, H);
-					member(sel, H);
-					member(nget, H);
-					member(exec, H).
-command([H|T], Rest) :- member('(', H),
-						command_sequence(T, Rest).
-command([H|T], T) :- member(')', H).
-command([H|T], T) :- member(eof, H).
-
 % Helpers
+
 get_tail([H|T], T).
+
+% Use nth1, and is list builtin
